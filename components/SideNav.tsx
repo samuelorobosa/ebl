1f05f0b8a795c1d0647c5c25e1b32cf151a5bb46
Samuel Amagbakhen <frontend_dev1@academystack.com>
2023-04-08 22:39:28 +0100

    Third commit

--- components/SideNav.tsx
+++ components/SideNav.tsx
@@ -4,7 +4,9 @@ import NavContext from "../context/Navigation/NavContext";
 import Link from "next/link";
 
 export default function SideNav (){
-    const {open, toggleNavbar} = useContext(NavContext);
+    const navContext = useContext(NavContext);
+    if(!navContext) return null;
+    const {open, toggleNavbar} = navContext;
     return(
         <nav className={`h-full ${open ? 'w-2/3':'w-0'} fixed z-50 top-0 right-0 bg-ebl-primary transition-all duration-500 overflow-hidden`}>
             <div className="text-white mt-10 flex justify-end mr-5">
@@ -13,7 +15,6 @@ export default function SideNav (){
             <div className="mt-5 text-2xl ml-5 text-white block">
                 <Link href="/about" className="py-4 pl-10 pr-12 block w-full whitespace-nowrap"> About Us</Link>
                 <Link href="/jointraining" className="py-4 pl-10 pr-12 block w-full whitespace-nowrap"> Join Training</Link>
-                <Link href="/contact" className="py-4 pl-10 pr-12 block w-full whitespace-nowrap"> Contact Us</Link>
             </div>
         </nav>
     )
